a8b4d36cdd2b18bf18ced334b47b2f0de262418b
azuka-bangkit <c181dsx0627@bangkit.academy>
2023-05-17 20:21:08 +0700

    Import firebase-functions

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+import functions from 'firebase-functions';
 import express from 'express';
 const app = express()
 const port = 3000
@@ -9,3 +10,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+exports.app = functions.https.onRequest(app);
