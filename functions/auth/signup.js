b37ec2f68b0b1770bd0416bc215103c72e7434ce
azuka-bangkit <c181dsx0627@bangkit.academy>
2023-05-30 22:38:21 +0700

    Try stringify

--- functions/auth/signup.js
+++ functions/auth/signup.js
@@ -4,11 +4,16 @@ import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
 
 const signup = async (req, res) => {
   try {
-    let response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgHBx142-JXJv9cvq08Nl5u2TgnCUGkMY/', {
+    const data = JSON.stringify({
       email: req.body.email,
       password: req.body.password,
       returnSecureToken: true
     });
+    let response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgHBx142-JXJv9cvq08Nl5u2TgnCUGkMY',
+      data,
+      {
+        headers: { 'Content-Type': 'application/json' }
+      });
 
     // Connect to firestore
     initializeApp();
