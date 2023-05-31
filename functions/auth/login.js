a3871f4fbe3f24630bbccc335b70dc9136eaff65
joh_rakatn <johanes.raka@ui.ac.id>
2023-05-31 12:08:25 +0700

    Implement login API with custom logic

---
+++ functions/auth/login.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const login = async (req) => {
+  try {
+    const data = JSON.stringify({
+      email: req.body.email,
+      password: req.body.password,
+      returnSecureToken: true
+    });
+    let response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCgHBx142-JXJv9cvq08Nl5u2TgnCUGkMY',
+      data,
+      {
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+    // Connect to firestore
+    initializeApp();
+    const db = getFirestore();
+
+    const docRef = db.collection('users').doc(response.data.localId);
+    const user = await docRef.get();
+    return [200, {
+            idToken: response.data.idToken,
+            refreshToken: response.data.refreshToken,
+            user: user
+    }]
+  } catch (e) {
+    console.error(e);
+    return [404, {
+      error: e
+    }]
+  }
+};
+
+
+export default login;
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 import functions from 'firebase-functions';
 import express from 'express';
 import signup from "./auth/signup.js";
+import login from "./auth/login.js";
 const app = express()
 const port = 3000
 
@@ -11,7 +12,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signup', async (req, res) => {
-  const r = await signup(req, res)
+  const r = await signup(req)
+  res.status(r[0]).send(r[1]);
+})
+
+app.post('/login', async (req, res) => {
+  const r = await login(req)
   res.status(r[0]).send(r[1]);
 })
 
