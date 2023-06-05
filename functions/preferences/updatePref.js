5e8464c1d2c91b5325a28d322bfeebfbbc8076fc
johanes-raka-tn <c181dsx2243@bangkit.academy>
2023-06-05 21:59:26 +0700

    Implement update preferences API

---
+++ functions/preferences/updatePref.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { getFirestore } from 'firebase-admin/firestore';
+
+const updatePref = async (req) => {
+  try {
+    const data = JSON.stringify({
+      idToken: req.body.idToken,
+    });
+    let response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${process.env.API_KEY}`,
+      data,
+      {
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+    // Connect to firestore
+    const db = getFirestore();
+
+    const docRef = db.collection('users').doc(response.data.users.localId);
+    await docRef.update({
+      preferences: {
+        workout_per_week: req.body.workout_per_week,
+        type_pref: req.body.type_pref,
+        train_duration: req.body.train_duration,
+        train_level: req.body.train_level
+      }
+    }).then(function() {
+        console.log("user's preferences updated");
+      });;
+    return [200, {
+      idToken: response.data.idToken,
+      refreshToken: response.data.refreshToken
+    }]
+
+  } catch (e) {
+    console.error(e);
+    return [400, {
+      error: e
+    }]
+  }
+};
+
+
+export default updatePref;
