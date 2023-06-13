8fdb70c2d95962c88cf70660023625829e40e2fa
johanes-raka-tn <c181dsx2243@bangkit.academy>
2023-06-14 00:36:29 +0700

    try mainifying

--- functions/model/food_recommender/main.py
+++ functions/model/food_recommender/main.py
@@ -11,12 +11,25 @@ from nltk.tokenize import word_tokenize
 def convert_to_string(data):
     return f"{data['diet_type']} {data['cuisine_type']} {data['protein']} {data['carbs']} {data['fat']} {data['calories']}"
 
+def mainify(obj):
+    """If obj is not defined in __main__ then redefine it in 
+    main so that dill will serialize the definition along with the object"""
+    if obj.__module__ != "__main__":
+        import __main__
+        import inspect
+        s = inspect.getsource(obj)
+        co = compile(s, '<string>', 'exec')
+        exec(co, __main__.__dict__)
+
 @functions_framework.http
 def foodrec_model(request):
   
     if request.method == 'POST':
         with open('foodrec_model.pkl','rb') as dill_file:
             loaded_model = dill.load(dill_file)
+        mainify(cosine_distances)
+        mainify(word_tokenize)
+        mainify(CountVectorizer)
         data = request.get_json()
         data_input = convert_to_string(data)
         result = loaded_model.recommend(data_input)
