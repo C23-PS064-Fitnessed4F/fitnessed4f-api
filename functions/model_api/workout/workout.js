7fc1a25b98587a483f2ca0b79128c914b68717e8
azuka-bangkit <c181dsx0627@bangkit.academy>
2023-06-13 20:58:52 +0700

    Check for 0

--- functions/model_api/workout/workout.js
+++ functions/model_api/workout/workout.js
@@ -12,6 +12,12 @@ const workout = async (req) => {
     const docRef = db.collection('users').doc(userId);
     const user = await docRef.get();
 
+    if (0 in Object.keys(user.data().workout_preferences)) {
+      return [400, {
+        error: "Workout preferences not configured yet"
+      }]
+    }
+
     let response = await axios.post('https://asia-southeast2-fitnessed4f.cloudfunctions.net/workout-model',
       user.data().workout_preferences,
       {
