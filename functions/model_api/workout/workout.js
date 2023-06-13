36c923d1a9bf47ef35402a68d656654cd1125e65
azuka-bangkit <c181dsx0627@bangkit.academy>
2023-06-13 20:24:40 +0700

    Implement workout API

---
+++ functions/model_api/workout/workout.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { getFirestore } from 'firebase-admin/firestore';
+import getUserId from "../../util/getUserId.js";
+
+const workout = async (req) => {
+  try {
+    const userId = await getUserId(req);
+
+    // Connect to firestore
+    const db = getFirestore();
+
+    const docRef = db.collection('users').doc(userId);
+    const user = await docRef.get();
+
+    let response = await axios.post('https://asia-southeast2-fitnessed4f.cloudfunctions.net/workout-model',
+      JSON.stringify(user['workout_preferences']),
+      {
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+    return [200, {
+      result: response.data.result,
+    }]
+  } catch (e) {
+    console.error(e);
+    return [404, {
+      error: e
+    }]
+  }
+};
+
+
+export default workout;
