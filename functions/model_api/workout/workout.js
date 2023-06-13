7f66a64ec13a4e1a565a01c2099a26ac20a0328c
azuka-bangkit <c181dsx0627@bangkit.academy>
2023-06-13 20:44:19 +0700

    Forgot to use .data()

--- functions/model_api/workout/workout.js
+++ functions/model_api/workout/workout.js
@@ -13,7 +13,7 @@ const workout = async (req) => {
     const user = await docRef.get();
 
     let response = await axios.post('https://asia-southeast2-fitnessed4f.cloudfunctions.net/workout-model',
-      user['workout_preferences'],
+      user.data().workout_preferences,
       {
         headers: { 'Content-Type': 'application/json' }
       });
