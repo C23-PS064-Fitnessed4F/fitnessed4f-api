ca85ee777cfc8d8f7e1aec7f8ba2e8daac9a2838
johanes-raka-tn <c181dsx2243@bangkit.academy>
2023-06-14 08:45:35 +0700

    try dill recurse = True

--- functions/model/food_recommender/main.py
+++ functions/model/food_recommender/main.py
@@ -4,9 +4,6 @@ import json
 import dill
 import nltk
 nltk.download('punkt')
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_distances
-from nltk.tokenize import word_tokenize
 
 def convert_to_string(data):
     return f"{data['diet_type']} {data['cuisine_type']} {data['protein']} {data['carbs']} {data['fat']} {data['calories']}"
@@ -27,8 +24,6 @@ def foodrec_model(request):
     if request.method == 'POST':
         with open('foodrec_model.pkl','rb') as dill_file:
             loaded_model = dill.load(dill_file)
-        mainify(cosine_distances)
-        mainify(word_tokenize)
         data = request.get_json()
         data_input = convert_to_string(data)
         result = loaded_model.recommend(data_input)
